09bd142617d37c9e80c0a2781ecf402886901ade
githhubio <117591134+githhubio@users.noreply.github.com>
2023-02-16 18:18:29 +0300

    Delete DepremData directory

--- DepremData/example.py
+++
@@ -1,4 +0,0 @@
-import lib.deprem as deprem
-deprem.deprep() # anlık verileri sadece bir kere yazar
-deprem.sondepreptarih() # anlık son deprem tarihini ve saatini yazar
-deprem.deprepolcer(5) # 5sn de bir anlık verileri yeniler / (5) = 5sn
