794914e477bc9147d4942ef4561f012b3579bf93
githhubio <117591134+githhubio@users.noreply.github.com>
2023-02-16 20:12:47 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 import lib.deprem as deprem
-deprem.deprep() # anlık verileri sadece bir kere yazar
+deprem.depremveri() # anlık verileri sadece bir kere yazar
 deprem.sondepreptarih() # anlık son deprem tarihini ve saatini yazar
 deprem.deprepolcer(5) # 5sn de bir anlık verileri yeniler / (5) = 5sn
