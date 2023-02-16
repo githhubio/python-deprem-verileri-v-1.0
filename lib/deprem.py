1b73d52f01a5ae8ac3096ad38b7284e2cec4f6ec
githhubio <117591134+githhubio@users.noreply.github.com>
2023-02-16 20:15:08 +0300

    Update deprem.py

--- lib/deprem.py
+++ lib/deprem.py
@@ -14,20 +14,20 @@ gelen_veri2 = soup.find_all("td")
 depremzamanı = (gelen_veri2[0].contents [len(gelen_veri2[0].contents)-2])
 
 
-def deprep():# anlık verileri sadece bir kere yazar
+def depremveri():# anlık verileri sadece bir kere yazar
  print("turkiye son 100 deprem data=")
  print("****************************************************")
  print(gelen_veri)
  print("****************************************************")
  print("veri boyutu: ",len(gelen_veri))
 
-def sondepreptarih():# anlık son deprem tarihini ve saatini yazar
+def sondepremtarih():# anlık son deprem tarihini ve saatini yazar
  print("turkiye son deprem zamanı data=")
  print("****************************************************")
  print(depremzamanı)
  print("****************************************************")
 
-def deprepolcer(xzamanbekle):#5sn de bir verileri yeniler
+def depremolcer(xzamanbekle):#5sn de bir verileri yeniler
  while(True):
    time.sleep(xzamanbekle)
    print("turkiye son 100 deprem data=")
